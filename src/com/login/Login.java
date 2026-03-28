1e11bc08811c23c0bf344e3370c5877c204970d5
Aniket <nawalkaraniket9@gmail.com>
2026-03-28 13:31:49 +0530

    i have added username and password

--- src/com/login/Login.java
+++ src/com/login/Login.java
@@ -1,5 +1,19 @@
 package com.login;
 
 public class Login {
+private String username = "aniket@4403" ; 
+private String password = "agn1234";
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
 
 }
