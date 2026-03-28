bdaa7ffcd92199a047997ba433dae5807d95008f
Aniket <nawalkaraniket9@gmail.com>
2026-03-28 13:34:03 +0530

    constructor added

--- src/com/login/Login.java
+++ src/com/login/Login.java
@@ -3,6 +3,12 @@ package com.login;
 public class Login {
 private String username = "aniket@4403" ; 
 private String password = "agn1234";
+
+public Login(String username, String password) {
+	super();
+	this.username = username;
+	this.password = password;
+}
 public String getUsername() {
 	return username;
 }
